d4db690bf9763614f245ae7c2b05a0441dd0b923
Florian Parfuss <Florian.parfuss@hotmail.de>
2020-01-07 11:40:20 +0100

    gpio test script

--- test.py
+++ test.py
@@ -2,6 +2,8 @@ import time
 import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(17, GPIO.OUT)
+GPIO.setup(17,GPIO.Out)
+GPIO.setup(17, GPIO.HIGH)
 for i in range(10):
     time.sleep(1)
+GPIO.setup(17, GPIO.LOW)

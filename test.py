b6aa8120ad6c57f80d88a63055a2f3b827f920f0
Florian Parfuss <Florian.parfuss@hotmail.de>
2020-01-07 11:48:08 +0100

    end.py added

--- test.py
+++ test.py
@@ -3,11 +3,10 @@ import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(17,GPIO.OUT)
-GPIO.setup(17, GPIO.HIGH)
-for i in range(10):
-    time.sleep(1)
 GPIO.setup(17, GPIO.LOW)
-for i in range(5):
-    time.sleep(5)
+for i in range(10):
+    print(i)
+    time.sleep(1)
+GPIO.setup(17, GPIO.HIGH)
 exit()
 

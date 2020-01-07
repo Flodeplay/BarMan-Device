d31140ec94d9280e7869cd550faacca2d6733290
Florian Parfuss <Florian.parfuss@hotmail.de>
2020-01-07 17:14:46 +0100

    end.py modified for every channel test.py modified for every channel

--- test.py
+++ test.py
@@ -1,12 +1,38 @@
 import time
 import RPi.GPIO as GPIO
 
+#Relai 17
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(17,GPIO.OUT)
 GPIO.setup(17, GPIO.LOW)
-for i in range(10):
+for i in range(3):
     print(i)
     time.sleep(1)
 GPIO.setup(17, GPIO.HIGH)
-exit()
 
+#27
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(27,GPIO.OUT)
+GPIO.setup(27, GPIO.LOW)
+for i in range(3):
+    print(i)
+    time.sleep(1)
+GPIO.setup(27, GPIO.HIGH)
+#22
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(22,GPIO.OUT)
+GPIO.setup(22, GPIO.LOW)
+for i in range(3):
+    print(i)
+    time.sleep(1)
+GPIO.setup(22, GPIO.HIGH)
+#23
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(23,GPIO.OUT)
+GPIO.setup(23, GPIO.LOW)
+for i in range(3):
+    print(i)
+    time.sleep(1)
+GPIO.setup(23, GPIO.HIGH)
+
+exit()

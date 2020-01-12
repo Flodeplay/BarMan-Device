a8cd5e8d41853dfe4232c82fced6085173c14ec0
Florian Parfuss <Florian.parfuss@hotmail.de>
2020-01-12 16:10:23 +0100

    now fullscreen

--- Main/View/application.py
+++ Main/View/application.py
@@ -7,11 +7,12 @@ class Application(Tk):
         self.geometry("800x400")
         self['bg'] = "#201F1E"
         self.frames = {}
-        # self.resizable(0, 0)
-        # self._geom='200x200+0+0'
-        # self.geometry("{0}x{0}+0+0".format(
-        # self.winfo_screenwidth(), self.winfo_screenheight()))
-        # self.bind('<Escape>',self.toggle_geom)
+        self.resizable(0, 0)
+        self._geom='200x200+0+0'
+        self.geometry("{0}x{0}+0+0".format(
+        self.winfo_screenwidth(), self.winfo_screenheight()))
+        self.bind('<Escape>',self.toggle_geom)
+        self.attributes('-fullscreen', True)
         Grid.columnconfigure(self, 0, weight=1)
         Grid.rowconfigure(self, 0, weight=1)
     def toggle_geom(self,event):

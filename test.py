4322d072fe05df107cd22f8ce562ff4255034354
Florian Parfuss <Florian.parfuss@hotmail.de>
2020-01-14 13:35:56 +0100

    test.py time now longer

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(17,GPIO.OUT)
 GPIO.setup(17, GPIO.LOW)
-for i in range(3):
+for i in range(6):
     print(i)
     time.sleep(1)
 GPIO.setup(17, GPIO.HIGH)
@@ -14,7 +14,7 @@ GPIO.setup(17, GPIO.HIGH)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(27,GPIO.OUT)
 GPIO.setup(27, GPIO.LOW)
-for i in range(3):
+for i in range(6):
     print(i)
     time.sleep(1)
 GPIO.setup(27, GPIO.HIGH)
@@ -22,17 +22,17 @@ GPIO.setup(27, GPIO.HIGH)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(22,GPIO.OUT)
 GPIO.setup(22, GPIO.LOW)
-for i in range(3):
+for i in range(6):
     print(i)
     time.sleep(1)
 GPIO.setup(22, GPIO.HIGH)
-#23
+#26
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(23,GPIO.OUT)
-GPIO.setup(23, GPIO.LOW)
-for i in range(3):
+GPIO.setup(26,GPIO.OUT)
+GPIO.setup(26, GPIO.LOW)
+for i in range(6):
     print(i)
     time.sleep(1)
-GPIO.setup(23, GPIO.HIGH)
+GPIO.setup(26, GPIO.HIGH)
 
 exit()
